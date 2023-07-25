a835a0db4f1c480c8d4068997ad7d88fa6b85bae
Andrey Shilin <ciousttx@gmail.com>
2023-07-25 12:17:09 +0300

    work test

--- conftest.py
+++ conftest.py
@@ -1,26 +1,39 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-@pytest.fixture(scope="function")
+from selenium.webdriver.chrome.options import Options
+'''@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
-    browser.quit()
+    browser.quit()'''
 
 '''@pytest.mark.parametrize('language', ["en", "ru"])
 def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")'''
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
 
 @pytest.mark.parametrize(scope="function", name='language', params=[
-    ('http://selenium1py.pythonanywhere.com/en', 'en')
-    ('http://selenium1py.pythonanywhere.com/ru', 'ru')
+    ('http://selenium1py.pythonanywhere.com/', 'en')
+    ('http://selenium1py.pythonanywhere.com/', 'ru')
 ])
 def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    browser.find_element(By.CSS_SELECTOR, "#login_link")'''
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
