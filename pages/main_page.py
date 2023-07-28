5427a23f9b415f2fe477835f260c71484dfb0d5a
krissssbro <Ciousttx@gmail.com>
2023-07-28 16:35:44 +0300

    4.2.8 Реализация LoginPage Main_page не работает проверка текста в текущем урл

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import MainPageLocators
 from .locators import LoginPageLocators
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class MainPage(BasePage):
@@ -15,8 +18,7 @@ class MainPage(BasePage):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        #assert "login" in MainPage.should_be_login_link()  # сообщение об ошибке
-        assert "login" in driver.current_url  # сообщение об ошибке
+        assert "login" in str(self.browser.current_url), "Login in url is not presented"
 
     def should_be_login_page(self):
         #assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login page is not presented"
